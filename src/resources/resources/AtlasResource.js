ceb4ded8f1bd9bd3dec536c7366d470d2f9edcbd
Vadim Postnikov <>
2020-05-31 16:43:25 +0300

    no message

--- src/resources/resources/AtlasResource.js
+++ src/resources/resources/AtlasResource.js
@@ -9,38 +9,36 @@ export default class AtlasResource extends DataResource {
         if (this._data) {
             this._textures = [];
 
-            /*let testData1 = [...this._data.matchAll(/(^.+$)\s(^(size|format|filter|repeat):\s*(.+)\s?)+(^.+$\s?(^\s+(rotate|xy|size|index|offset|orig):\s*(.+)\s?)+)+/mig)];
-            testData1.forEach((data)=>{
-                let testData2 = [...data[0].matchAll(/(^.+$)\s(^(size|format|filter|repeat):\s*(.+)\s?)+/mig)];
-                console.log('testData2', testData2);
-
-                let testData3 = [...data[0].matchAll(/(^.+$\s?(^\s+(rotate|xy|size|index|offset|orig):\s*(.+)\s?))+/mig)];
-                console.log('testData3', testData3);
-            })*/
-
-            let regExpSpriteSheets = /(^.+$\s?)(^(size|format|filter|repeat).+\s?)+((^.+$\s?)(^\s+(rotate|xy|size|index|offset|orig).+\s?)+)+/mig;
-            let matchSpriteSheetsString = this._data.match(regExpSpriteSheets);
-            matchSpriteSheetsString.forEach((spriteSheetsStringItem) => {
-
-                let size = spriteSheetsStringItem.match(/^(?<size>size):\s*(?<width>\d*).\s*(?<height>\d*)$/mi).groups
+            [...this._data.matchAll(/(.+\s)+/mig)].forEach((data) => {
+                let testData2 = data[0].match(/(^.+$)\s(^(size|format|filter|repeat):\s*(.+)\s)+/mig);
                 let textureData = {
-                    name: spriteSheetsStringItem.match(/(?<name>^.+.(png|jpg)$)/mi).groups.name,
+                    name: '',
                     size: {
-                        width: +size.width,
-                        height: +size.height,
+                        width: '',
+                        height: ''
                     }
-                }
+                };
+                textureData.name = testData2[0].match(/^(.[^:]+)$/mig)[0];
 
-                let regExpSprites = /(^.+$\s?)(^\s+(rotate|xy|size|index|offset|orig).+\s?)+/mig;
-                let matchSpritesString = spriteSheetsStringItem.match(regExpSprites);
-                matchSpritesString.forEach((matchSpritesStringItem) => {
-                    //console.log(matchSpritesStringItem);
+                let values = null;
+                [...testData2[0].matchAll(/^(.+):\s*(.+)$/mig)].forEach((paramData) => {
+                    switch (paramData[1]) {
+                        case 'size':
+                            values = paramData[2].split(',');
+                            textureData.size.width = +(values[0].trim());
+                            textureData.size.height = +(values[1].trim());
+                            break;
+                    }
+                });
+
+                [...data[0].matchAll(/(^.+$)\s?(^\s+(rotate|xy|size|index|offset|orig):\s*(.+)\s?)+/mig)].forEach((data) => {
+                    let spriteName = data[0].match(/^(.[^:]+)$/mig)[0];
                     let spriteData = {
                         frame: {
-                            x:0,    //xy[0]
-                            y:0,    //xy[1]
-                            w:0,    //size[0]
-                            h:0     //size[1]
+                            x: 0,    //xy[0]
+                            y: 0,    //xy[1]
+                            w: 0,    //size[0]
+                            h: 0     //size[1]
                         },
                         rotation: 0,
                         rotated: false, //rotate
@@ -60,12 +58,9 @@ export default class AtlasResource extends DataResource {
                             y: 1
                         },
                         textureName: textureData.name
-                    }
+                    };
 
-                    let spriteName = matchSpritesStringItem.match(/(?<name>^.+$)/mi).groups.name;
-                    let testData = [...matchSpritesStringItem.matchAll(/^\s+(rotate|xy|size|index|offset|orig):\s*(.+)$/mig)];
-                    let values = null;
-                    testData.forEach((paramData)=>{
+                    [...data[0].matchAll(/^\s*(.+):\s(.+)$/mig)].forEach((paramData) => {
                         switch (paramData[1]) {
                             case 'rotate':
                                 spriteData.rotated = paramData[2] === 'true';
@@ -94,104 +89,11 @@ export default class AtlasResource extends DataResource {
                                 spriteData.spriteSourceSize.y = +(values[1].trim());
                                 break;
                         }
-                    })
+                    });
                     this._sprites[spriteName] = spriteData;
                 });
-
                 this._textures.push(textureData);
             });
-
-            /*let regExpSpriteSheets = /(^.+$\s?)(^(size|format|filter|repeat).+\s?)+((^.+$\s?)(^\s+(rotate|xy|size|index|offset|orig).+\s?)+)+/mig;
-            let matchSpriteSheetsString = this._data.match(regExpSpriteSheets);
-            matchSpriteSheetsString.forEach((spriteSheetsStringItem) => {
-
-                let size = spriteSheetsStringItem.match(/^(?<size>size):\s*(?<width>\d*).\s*(?<height>\d*)$/mi).groups
-                let textureData = {
-                    name: spriteSheetsStringItem.match(/(?<name>^.+.(png|jpg)$)/mi).groups.name,
-                    size: {
-                        width: +size.width,
-                        height: +size.height,
-                    }
-                }
-
-                let regExpSprites = /(^.+$\s?)(^\s+(rotate|xy|size|index|offset|orig).+\s?)+/mig;
-                let matchSpritesString = spriteSheetsStringItem.match(regExpSprites);
-                matchSpritesString.forEach((matchSpritesStringItem) => {
-                    //console.log(matchSpritesStringItem);
-
-                    let spriteData = {
-                        frame: {
-                            x:0,    //xy[0]
-                            y:0,    //xy[1]
-                            w:0,    //size[0]
-                            h:0     //size[1]
-                        },
-                        rotation: 0,
-                        rotated: false, //rotate
-                        trimmed: true,
-                        spriteSourceSize: {
-                            x: 0,   //offset[0]
-                            y: 0,   //offset[1]
-                            w: 0,   //size[0]
-                            h: 0    //size[1]
-                        },
-                        sourceSize: {
-                            w: 0,   //orig[0]
-                            h: 0    //orig[1]
-                        },
-                        scale: {
-                            x: 1,
-                            y: 1
-                        },
-                        textureName: textureData.name
-                    }
-
-                    let spriteName = matchSpritesStringItem.match(/(?<name>^.+$)/mi).groups.name;
-                    let testData = [...matchSpritesStringItem.matchAll(/^\s+(rotate|xy|size|index|offset|orig):\s*(.+)$/mig)];
-                    let values = null;
-                    testData.forEach((paramData)=>{
-                        switch (paramData[1]) {
-                            case 'rotate':
-                                spriteData.rotated = paramData[2] === 'true';
-                                spriteData.rotation = spriteData.rotated ? 90 : 0;
-                                break;
-                            case 'xy':
-                                values = paramData[2].split(',');
-                                spriteData.frame.x = +(values[0].trim());
-                                spriteData.frame.y = +(values[1].trim());
-                                break;
-                            case 'orig':
-                                values = paramData[2].split(',');
-                                spriteData.sourceSize.w = +(values[0].trim());
-                                spriteData.sourceSize.h = +(values[1].trim());
-                                break;
-                            case 'size':
-                                values = paramData[2].split(',');
-                                spriteData.frame.w = +(values[0].trim());
-                                spriteData.frame.h = +(values[1].trim());
-                                spriteData.spriteSourceSize.w = +(values[0].trim());
-                                spriteData.spriteSourceSize.h = +(values[1].trim());
-                                break;
-                            case 'offset':
-                                values = paramData[2].split(',');
-                                spriteData.spriteSourceSize.x = +(values[0].trim());
-                                spriteData.spriteSourceSize.y = +(values[1].trim());
-                                break;
-                        }
-                    })
-                    this._sprites[spriteName] = spriteData;
-                });
-
-                this._textures.push(textureData);
-            });*/
-
-            /*let regExp = new RegExp(/((^.+$\s)(^\s+((rotate|xy|size|index|offset|orig).+)\s?)+)/mig);
-            let test = null;
-            while(test = regExp.exec(this._data)) {
-                console.log(test);
-            }*/
-
-            //console.log(this._sprites);
         }
     }
 
